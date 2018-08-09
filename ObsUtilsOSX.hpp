1ca65918a94aa931a126c9220773c312f34e3463
DungFu <dungfu3@gmail.com>
2018-08-09 02:17:04 -0700

    mac totally works now!

---
+++ ObsUtilsOSX.hpp
@@ -0,0 +1,12 @@
+#ifndef OBSUTILSOSX_H
+#define OBSUTILSOSX_H
+
+#include <set>
+#include <string>
+
+namespace ObsUtilsOSX
+{
+    std::set<std::string> getOpenApps();
+}
+
+#endif // OBSUTILSOSX_H
